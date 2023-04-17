48c665f16a8dbd70d6be16084ac50ff3f08b81d0
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-17 21:47:22 +0700

    init proj

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen background-color">
+    <div className = "fixed w-64 h-screen sidebar-purple">
+        <div className="m-4">
+        <button type="button" className="py-2.5 px-5 mr-2 mb-2 w-56 text-xs font-medium text-purple-700 bg-transparent rounded-lg border border-purple-700 focus:ring-purple-500 focus:border-purple-500 block hover:border-purple-950 hover:text-purple-950">+ New Chat</button>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+}
