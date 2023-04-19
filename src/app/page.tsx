f579c8327a78d4324c8b09445149879d20e715af
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-19 20:25:23 +0700

    fix className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,25 @@ export default function Home() {
             + New Chat
           </button>
         </div>
-        <hr class="w-56 h-0.5 mx-auto my-4 bg-purple-900 border-0 rounded md:my-4 dark:bg-gray-700"/>
+        <hr className="w-56 h-0.5 mx-auto my-4 bg-purple-900 border-0 rounded md:my-4 dark:bg-gray-700"/>
         <div className = "m-4 h-48 space-y-3">
         <div className="m-3 text-purple-950">Algorithm</div>
-        <div class="flex gap-10">
-        <div class="inline-flex items-center">
+        <div className="flex gap-10">
+        <div className="inline-flex items-center">
           <label
             className="relative flex cursor-pointer items-center rounded-full p-3"
-            for="html"
             data-ripple-dark="true"
           >
             <input
               id="html"
               name="type"
               type="radio"
-              class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-purple-500 text-purple-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-purple-500 checked:before:bg-purple-500 hover:before:opacity-10"
+              className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-purple-500 text-purple-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-purple-500 checked:before:bg-purple-500 hover:before:opacity-10"
             />
-            <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-purple-500 opacity-0 transition-opacity peer-checked:opacity-100">
+            <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-purple-500 opacity-0 transition-opacity peer-checked:opacity-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-3.5 w-3.5"
+                className="h-3.5 w-3.5"
                 viewBox="0 0 16 16"
                 fill="currentColor"
               >
@@ -39,7 +38,6 @@ export default function Home() {
           </label>
           <label
             className="mt-px cursor-pointer select-none text-purple-900"
-            for="html"
           >
             KMP
           </label>
@@ -47,7 +45,6 @@ export default function Home() {
         <div className="inline-flex items-center">
           <label
             className="relative flex cursor-pointer items-center rounded-full p-3"
-            for="react"
             data-ripple-dark="true"
           >
             <input
@@ -69,7 +66,6 @@ export default function Home() {
           </label>
           <label
             className="mt-px cursor-pointer  text-purple-900"
-            for="react"
           >
             BM
           </label>
