da882bb24845252d8d75bd7af65c299bb125ad8b
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-20 03:11:58 +0700

    fix fe

---
+++ src/app/components/question.tsx
@@ -0,0 +1,16 @@
+import Image from "./pub"
+
+const Question = () => {
+  return (
+    <div className="grid grid-cols-12 bg-purple-200 rounded-md">
+      <div className="col-span-1 bg-purple-500 mr-auto rounded-md p-2">
+        <img src="/gojobot.jpg" width={30} height={30} alt="profile"/>
+      </div>
+      <div className="col-span-11 px-4 flex flex-col justify-center">
+        <span className="text-sm text-purple-950">imagine kelar</span>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
