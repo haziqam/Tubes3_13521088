4a5df3b0e8e4036a27435e8437a0f92bb07e9b87
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-04-20 16:26:37 +0700

    fix route

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-
-export default function handler(req: NextApiRequest, res: NextApiResponse){
-  res.status(200).json({})
+export async function GET(request: Request) {
+  return new Response('Hello, Next.js!')
 }
