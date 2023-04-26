ee1f9664bd4ece71c2ad83bf8740af9f115891eb
Haziq <haziqabiyyu@gmail.com>
2023-04-26 10:44:25 +0700

    featureClassifier

--- src/app/feature/featureClassifier.ts
+++ src/app/feature/featureClassifier.ts
@@ -42,8 +42,11 @@ class FeatureClassifier {
 }
 
 // Example case
-const userMessage = "Tambahkan pertanyaan XXX dengan jawaban YYY ke dalam database.";
+const userMessage = "Tambahkan pertanyaan apa ibukota Indonesia dengan jawaban YYY ke dalam database.";
 const userMessage1 = "Hapus pertanyaan XXX dari database";
-const classifier = new FeatureClassifier(userMessage1);
-const feature = classifier.getFeature();
-console.log(feature.getResponse());
+let addQuestion: RegExp 
+= /^(tambahkan|input|masukkan|simpan) (pertanyaan|query) (.*) dengan (jawaban|respon) (.*) ke (dalam )?(database|basis data)/i; 
+console.log(addQuestion.exec(userMessage));
+// const classifier = new FeatureClassifier(userMessage1);
+// const feature = classifier.getFeature();
+// console.log(feature.getResponse());
