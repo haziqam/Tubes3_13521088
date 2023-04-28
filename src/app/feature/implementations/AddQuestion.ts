e67e147f710707db0accf8c6608253633d9905f8
Haziq <haziqabiyyu@gmail.com>
2023-04-28 23:14:42 +0700

    Test db feature on AddQuestion

--- src/app/feature/implementations/AddQuestion.ts
+++ src/app/feature/implementations/AddQuestion.ts
@@ -1,33 +1,65 @@
 import { Feature } from "../feature";
+import { FeatureClassifier } from "../featureClassifier"
+import prisma from '../prismaClient';
+
+
 
 export class AddQuestion extends Feature {
     private readonly regexMatch: string[];
+
+    addQuestionToDB = async () => {
+        const addedQnA = await prisma.questionandAnswer.create({
+            data: {
+                question: this.extractQuestion(),
+                answer: this.extractAnswer()
+            }
+            
+        });
+        //console.log(addedQnA);
+    }
 
     constructor (regexMatch: string[]) {
         super();
         this.regexMatch = regexMatch;
     }
 
-    private getQuestion(): string {
+    // Extracts question from regex match
+    private extractQuestion(): string {
         return this.regexMatch[3];
     }
 
-    private getAnswer(): string {
+    // Extracts answer from regex match
+    private extractAnswer(): string {
         return this.regexMatch[5];
     }
 
     getResponse(): string {
-        const question: string = this.getQuestion();
-        const answer: string = this.getAnswer();
-
-        /* Adds question and answser to db */
+        const question: string = this.extractQuestion();
+        const answer: string = this.extractAnswer();
 
         let successful: boolean = true;
+        /* Adds question and answser to db */
+        this.addQuestionToDB()
+            .catch(e => {
+                console.error(e.message);
+                successful = false;
+            })
+            .finally(async () => {
+                await prisma.$disconnect();
+            });
+
+        
         if (successful) {
-            return "Pertanyaan " + question + " dengan jawaban " + answer + "berhasil ditambahkan!";
+            return "Pertanyaan " + question + " dengan jawaban " + answer + " berhasil ditambahkan!";
         }
         else {
             return "gagal"
         }
     }
 }
+
+const userMsg = "Tambahkan pertanyaan apa matkul terseru dengan jawaban STIMA ke database";
+const classsifier = new FeatureClassifier(userMsg);
+const feature = classsifier.getFeature();
+console.log(feature.getResponse());
+
