2efa6bc865cb4b71b7c58b14bf403f3f0d268bfa
Haziq <haziqabiyyu@gmail.com>
2023-05-02 23:07:44 +0700

    Add API to update answer in db and delete qna from db

---
+++ src/app/algorithm/findOccurrence.ts
@@ -0,0 +1,14 @@
+function findOccurrence() {
+    /**
+     * 
+        params berupa text yang dicari, list of qna yang sudah difetch, dan algoritma yang digunakan (kmp/bm)
+        return berupa 
+        {
+            exactMatch: string?
+            highestMatch: {
+                [{qna1, percentage1}, {qna2, percentage2}, {qna3, percentage3}]]
+            }?
+        }
+     * 
+     */
+}
--- src/app/algorithm/question.ts
+++ src/app/algorithm/question.ts
@@ -1,9 +1,119 @@
 import { QuestionAndAnswer } from "./interface";
 export async function getQnA(): Promise<QuestionAndAnswer[]> {
-    const res = await fetch(`http://localhost:3000/api/post/getQnA`);
-    if (!res.ok) {
-      throw new Error('Failed to fetch data from API');
-    }
-    const data = await res.json();
-    return data;
+  const res = await fetch(`http://localhost:3000/api/post/getQnA`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data from API');
+  }
+  const data = await res.json();
+  return data;
 }  
+
+
+/**
+ * @param question Pertanyaan yang akan ditambahkan
+ * @param answer Jawaban yang akan ditambahkan
+ * @returns Data QuestionAndAnswer {id, question, answer} yang berhasil ditambahkan
+ */
+export async function addQnA(question: string, answer: string): Promise<QuestionAndAnswer> {
+  // Memanggil API addQnA dengan memasukkan parameter question dan answer ke dalam NextApiRequest body
+  const res = await fetch(`http://localhost:3000/api/post/addQnA`, {
+              method: 'POST',
+              body: JSON.stringify({param: 
+                {
+                  question: question,
+                  answer: answer
+                }
+              })
+  });
+
+  if (!res.ok) {
+    const ApiResponseMsg = await res.text();
+    throw new Error('Failed to add data using API: ' + ApiResponseMsg);
+  }
+
+  const addedQnA = await res.json();
+  return addedQnA;
+}
+
+
+export async function updateAnswer(id: number, newAnswer: string): Promise<QuestionAndAnswer> {
+    // Memanggil API updateAnswer dengan memasukkan parameter id dan newAnswer ke dalam NextApiRequest body
+    const res = await fetch(`http://localhost:3000/api/post/updateAnswer`, {
+      method: 'PUT',
+      body: JSON.stringify({param: 
+        {
+          id: id,
+          newAnswer: newAnswer
+        }
+      })  
+    });
+
+    if (!res.ok) {
+      const ApiResponseMsg = await res.text();
+      throw new Error('Failed to update data using API: ' + ApiResponseMsg);
+    }
+
+    const updatedQnA = await res.json();
+    return updatedQnA;
+}
+
+
+
+export async function deleteQnA(id: number): Promise<QuestionAndAnswer> {
+    // Memanggil API deleteAnswer dengan memasukkan parameter id dan newAnswer ke dalam NextApiRequest body
+    const res = await fetch(`http://localhost:3000/api/post/deleteQnA`, {
+      method: 'PUT',
+      body: JSON.stringify({param: 
+        {
+          id: id
+        }
+      })  
+    });
+
+    if (!res.ok) {
+      const ApiResponseMsg = await res.text();
+      throw new Error('Failed to delete data using API: ' + ApiResponseMsg);
+    }
+
+    const deletedQnA = await res.json();
+    return deletedQnA;
+}
+
+
+
+// let test: QuestionAndAnswer[];
+// getQnA().then((allQnA) => {
+//   test = allQnA;
+//   console.log(test);
+// })
+
+// try {
+//   updateAnswer(10, "hhhhhhhhh")
+//     .then((updatedAnswer) => {
+//       console.log(updatedAnswer)
+//     })
+// }
+// catch (error) {
+//   console.log(error)
+// }
+
+// try {
+//   addQnA("asdf", "jkl;")
+//     .then((addedData) => {
+//       console.log(addedData);
+//     });
+// }
+// catch (error) {
+//   console.error(error);
+// };
+
+// try {
+//   deleteQnA(1)
+//     .then((deletedData) => {
+//       console.log(deletedData);
+//     })
+// }
+// catch (error) {
+//   console.error(error);
+// };
+
