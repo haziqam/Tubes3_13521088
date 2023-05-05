9957792dd36029e76e3880c63a1689e8c1e6e339
Haziq <haziqabiyyu@gmail.com>
2023-05-05 21:13:42 +0700

    minor change on add/delete question format

--- src/app/feature/implementations/Help.ts
+++ src/app/feature/implementations/Help.ts
@@ -10,15 +10,15 @@ export class Help extends Feature {
 
         2. Menambahkan pertanyaan ke databse
         Untuk menambahkan pertanyaan ke database, berikut adalah beberapa masukan yang diterima:
-        Tambahkan pertanyaan XXX dengan jawaban YYY ke dalam database
-        Simpan query XXX dengan jawaban YYY ke database
-        Masukkan pertanyaan XXX dengan respon YYY ke dalam basis data
+        Tambahkan pertanyaan "XXX" dengan jawaban "YYY" ke dalam database
+        Simpan query "XXX" dengan jawaban "YYY" ke database
+        Masukkan pertanyaan "XXX" dengan respon "YYY" ke dalam basis data
 
         3. Menghapus pertanyaan dari database
         Untuk menghapus pertanyaan ke database, berikut adalah beberapa masukan yang diterima:
-        Hapus pertanyaan XXX dari database
-        Hilangkan informasi query XXX dari database
-        Hapus data pertanyaan XXX dari basis data
+        Hapus pertanyaan "XXX" dari database
+        Hilangkan informasi query "XXX" dari database
+        Hapus data pertanyaan "XXX" dari basis data
 
         4. Kalkulator
         Anda dapat menggunakan fitur ini untuk melakukan perhitungan matematis yang melibatkan
